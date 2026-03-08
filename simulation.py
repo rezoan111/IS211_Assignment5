1f9fc1c3d0138b406c96a02e9e951e2a100a123a
rezoan111 <rezoan111@gmail.com>
2026-03-08 01:33:23 -0500

    simulation.py

--- simulation.py
+++ simulation.py
@@ -1,1 +1,5 @@
 # IS211 Assignment 5
+# This program will simulate network requests
+# First I will do one server simulation
+# Then I will do multiple server simulation
+# Main part will read the arguments from command line
