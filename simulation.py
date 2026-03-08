dc7a0d1fad9c2e7b2a10b802bcee55bd9e7a1e54
rezoan111 <rezoan111@gmail.com>
2026-03-08 01:36:24 -0500

    simulation.py

--- simulation.py
+++ simulation.py
@@ -3,3 +3,25 @@
 # First I will do one server simulation
 # Then I will do multiple server simulation
 # Main part will read the arguments from command line
+
+# Request class
+# This class will store request time, file path, and processing time
+
+# Server class
+# This class will handle the current request and processing time left
+
+# One server simulation
+# This part will read the csv file
+# This part will put requests in one queue
+# This part will calculate average wait time
+
+# Many servers simulation
+# This part will use more than one server
+# Requests will go to servers in round robin order
+# This part will also calculate average wait time
+
+# Main function
+# This part will read --file
+# This part will also read --servers if given
+# If no --servers then run one server simulation
+# If --servers is given then run many servers simulation
